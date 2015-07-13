2f643bf859ab5f0435eaee5b71fedfb7a5b9792f
KypT <0KypT0@gmail.com>
2015-07-13 19:15:18 +0500

    fixed bug with assets in production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@ Rails.application.config.assets.version = '1.0'
 controllers = %w( products pages cart )
 
 controllers.each do | controller |
-  Rails.application.config.assets.precompile += %W( #{controller}.js #{controller}.css )
+  Rails.application.config.assets.precompile += %W( #{controller}.js )
 end
 
 Rails.application.config.assets.precompile += %w( admin.js )
