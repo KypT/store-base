44a4bc3cf0a05eae379bd0710106242d504f9bf0
KypT <0KypT0@gmail.com>
2015-08-22 18:47:24 +0500

    site update

---
+++ app/controllers/specials_controller.rb
@@ -0,0 +1,58 @@
+class SpecialsController < ApplicationController
+  before_action :set_special, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @specials = Special.all
+  end
+
+  def show
+    @id = params[:id].to_i
+    @specials = Special.order(:id).reject { |cat| cat.id <= @id }
+    @special = @specials.empty? ? Special.order(:id).first : @specials.first
+    render :layout => false
+  end
+
+  def new
+    @special = Special.new
+  end
+
+  def edit
+  end
+
+  def create
+    @special = Special.new(special_params)
+    if @special.save
+      redirect_to @special, notice: 'Special was successfully created.'
+    else
+      render :edit
+    end
+  end
+
+  def update
+    add_image if params[:image]
+    if @special.update(special_params)
+      redirect_to specials_path, notice: 'Special was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @special.destroy
+    redirect_to specials_url, notice: 'Special was successfully destroyed.'
+  end
+
+  private
+  def add_image
+    @special.image = Image.new(file: params[:image])
+  end
+
+  def set_special
+    @special = Special.find(params[:id])
+  end
+
+  def special_params
+    params.require(:special).permit(:title, :description)
+  end
+
+end
